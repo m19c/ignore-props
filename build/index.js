6205d998d99bc7174162025c5e8a7f381a801899
Marc Binder <marc.binder@boerse-go.de>
2016-04-18 13:15:57 +0200

    Add the first build

--- build/index.js
+++ build/index.js
@@ -1,5 +1,32 @@
-const ignoreProps = (props, ...ignore) => {
-  console.log(props, ignore);
-};
+"use strict";
 
-console.log(ignoreProps({ id: 1, className: 'some value', anotherProps: true }, 'id'));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function flatten(array) {
+  return array.reduce(function (previous, current) {
+    var items = Array.isArray(current) ? flatten(current) : [current];
+    return previous.concat(items);
+  }, []);
+}
+
+exports.default = function (props) {
+  for (var _len = arguments.length, ignore = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    ignore[_key - 1] = arguments[_key];
+  }
+
+  var key = void 0;
+
+  props = Object.assign({}, props);
+  ignore = flatten(ignore);
+
+  for (key in props) {
+    if (ignore.indexOf(key) === -1) {
+      continue;
+    }
+
+    delete props[key];
+  }
+
+  return props;
+};
